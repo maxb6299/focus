8c68abd99d13588a4f7dd5f23e86cf65a7157b73
Max <126419161+maxb6299@users.noreply.github.com>
2023-06-17 14:42:31 -0400

    Add basics to server.js

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
+
+app.use(cors());
+
+app.listen(3000, () => console.log(`Server started. Listening on port 3000.`));
