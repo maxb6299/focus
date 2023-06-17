38d7b72d12bca6175602fa92a57fb1cb8c73e167
Max <126419161+maxb6299@users.noreply.github.com>
2023-06-17 17:39:47 -0400

    Uninstall mongoose Install mongodb Set up connection to database

--- server/server.js
+++ server/server.js
@@ -2,10 +2,34 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 
 app.use(cors());
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.DATABASE_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Successfully connected to database");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(process.env.PORT, () =>
   console.log(`Server started. Listening on port ${process.env.PORT}.`)
