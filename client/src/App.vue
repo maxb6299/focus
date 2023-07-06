578ba87b71628bc8c600d3c398f11b3b71c7f873
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-06 10:48:43 -0400

    Implement music player

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="main">
+    <div></div>
     <Timer></Timer>
+    <MusicPlayer></MusicPlayer>
   </div>
 </template>
 
 <script>
+import MusicPlayer from './components/MusicPlayer.vue'
+import Navbar from './components/Navbar.vue'
 import Timer from './components/Timer.vue'
 
 export default {
   name: 'App',
   components: {
+    MusicPlayer,
     Timer
   }
 }
---
+++ client/src/components/MusicPlayer.vue
@@ -0,0 +1,13 @@
+<template>
+    <iframe width="400" :src=musicLink></iframe>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                musicLink: 'https://www.youtube-nocookie.com/embed/Hlp6aawXVoY'
+            }
+        }
+    }
+</script>
