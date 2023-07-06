d8dac06b6261b093ce7951a6f928564bc3347cf5
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-06 10:50:14 -0400

    Add some of the string parsing methods for links

--- client/src/components/MusicPlayer.vue
+++ client/src/components/MusicPlayer.vue
@@ -8,6 +8,47 @@
             return {
                 musicLink: 'https://www.youtube-nocookie.com/embed/Hlp6aawXVoY'
             }
+        },
+
+        methods: {
+            getWebsiteName(link) {
+                var websiteNameStartIndex; // index that name begins in string
+
+                // so string skips 'https://'
+                for (var i = 0; i < 20; i++) { 
+                    if (link.substring(i, i + 2) == '//') {
+                        websiteNameStartIndex = i + 2;
+                    }
+                }
+                // so string skips 'www.'
+                if (link.substring(websiteNameStartIndex, websiteNameStartIndex + 4) == 'www.') {
+                    websiteNameStartIndex += 4;
+                }
+
+                var websiteNameEndIndex = websiteNameStartIndex;
+                while (true) {
+                    if (link[websiteNameEndIndex] == '.') {
+                        break;
+                    }
+                    websiteNameEndIndex++;
+                }
+
+                var websiteName = link.substring(websiteNameStartIndex, websiteNameEndIndex);
+                return websiteName;
+            },
+            configureLink() {           
+                var websiteName = this.getWebsiteName(this.musicLink);
+
+                if (websiteName == 'youtube') this.configureYoutube;
+                else if (websiteName == 'soundcloud') this.configureSoundcloud;
+
+            },
+            configureYoutube() {
+
+            },
+            configureSoundcloud() {
+
+            }
         }
     }
 </script>
