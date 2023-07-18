63410254c30fb0e579a9800843e9afe3bbecc472
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-18 12:13:49 -0400

    Import new components Implement Button component Make a navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="main">
-    <div></div>
-    <Settings @get-cookie="getCookie"></Settings>
+    <div class="navbar">
+      <Button :buttonName="'Logo'"></Button>
+      <Button :buttonName="'About'"><About></About></Button>
+      <div style="width:64px; height:64px;"></div>
+      <Button :buttonName="'Account'"><Account></Account></Button>
+      <Button :buttonName="'Settings'"><Settings @get-cookie="getCookie"></Settings></Button>
+    </div>
+    
     <Timer :settings="settings.timerSettings"></Timer>
     <MusicPlayer :settings="settings.musicSettings"></MusicPlayer>
   </div>
 </template>
 
 <script>
+import About from './components/About.vue'
+import Account from './components/Account.vue'
+import Button from './components/Button.vue'
 import MusicPlayer from './components/MusicPlayer.vue'
 import Settings from './components/Settings.vue'
 import Timer from './components/Timer.vue'
@@ -15,6 +24,9 @@ import Timer from './components/Timer.vue'
 export default {
   name: 'App',
   components: {
+    About,
+    Account,
+    Button,
     MusicPlayer,
     Settings,
     Timer
@@ -37,6 +49,7 @@ export default {
 
   created() {
     document.title = "Focus";
+    this.getCookie();
   }
 }
 </script>
@@ -47,5 +60,9 @@ export default {
     flex-direction: column;
     align-items: center;
     gap: 150px
+  }
+  .navbar {
+    display: flex;
+    gap: 16px;
   }
 </style>
