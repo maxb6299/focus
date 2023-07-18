6ee6c291311b0379f59cb43299a95b516324f4dc
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-18 11:08:13 -0400

    Write button component

---
+++ client/src/components/Button.vue
@@ -0,0 +1,35 @@
+<template>
+    <img 
+    class="icon" :src="`/assets/icons/${this.buttonName}.png`"
+    @click="toggleButton" 
+    :alt="buttonName">
+
+    <div v-if="isButtonOn">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['buttonName', 'isSmall'],
+
+        data() {
+            return {
+                isButtonOn: false
+            }
+        },
+        
+        methods: {
+            toggleButton() {
+                this.isButtonOn = !this.isButtonOn;
+            }
+        }
+    }
+</script>
+
+<style>
+    .icon { 
+        width: 64px;
+        height: 64px;
+    }
+</style>
