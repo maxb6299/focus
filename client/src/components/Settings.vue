ebbb32c55021eb933691eec8ca320fd9a20c821a
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-18 12:03:52 -0400

    Remove settings button from Settings component

--- client/src/components/Settings.vue
+++ client/src/components/Settings.vue
@@ -1,37 +1,34 @@
 <template>
     <div>
-        <button @click="toggleSettings">Settings</button>
-        <div v-if="viewSettings">
-            <form @submit.prevent="saveSettings">
-                        Timer: <br>
-                        Time For Work: <input required 
-                        v-model="settings.timerSettings.workMinutes" 
-                        type="number" min="1" step="1"
-                        placeholder="25"> <br>
+        <form @submit.prevent="saveSettings">
+                    Timer: <br>
+                    Time For Work: <input required 
+                    v-model="settings.timerSettings.workMinutes" 
+                    type="number" min="1" step="1"
+                    placeholder="25"> <br>
 
-                        Time For Break: <input required 
-                        v-model="settings.timerSettings.breakMinutes" 
-                        type="number" min="1" step="1"
-                        placeholder="5"> <br>
+                    Time For Break: <input required 
+                    v-model="settings.timerSettings.breakMinutes" 
+                    type="number" min="1" step="1"
+                    placeholder="5"> <br>
 
-                        Time For Long Break: <input required 
-                        v-model="settings.timerSettings.longBreakMinutes" 
-                        type="number" min="1" step="1"
-                        placeholder="5"> <br>
+                    Time For Long Break: <input required 
+                    v-model="settings.timerSettings.longBreakMinutes" 
+                    type="number" min="1" step="1"
+                    placeholder="5"> <br>
 
-                        Long Break Interval: <input required 
-                        v-model="settings.timerSettings.longBreakInterval" 
-                        type="number" min="1" step="1"
-                        placeholder="5"> <br>
-            
-                        Music: <br>
-                        Music Link: <input required 
-                        v-model="settings.musicSettings.musicLink" 
-                        type="url"
-                        placeholder="https://www.youtube.com/watch?v=Hlp6aawXVoY"><br>
-                <input type="submit" value="Save">
-            </form>
-        </div>
+                    Long Break Interval: <input required 
+                    v-model="settings.timerSettings.longBreakInterval" 
+                    type="number" min="1" step="1"
+                    placeholder="5"> <br>
+        
+                    Music: <br>
+                    Music Link: <input required 
+                    v-model="settings.musicSettings.musicLink" 
+                    type="url"
+                    placeholder="https://www.youtube.com/watch?v=Hlp6aawXVoY"><br>
+            <input type="submit" value="Save">
+        </form>
     </div>
 </template>
 
@@ -39,8 +36,6 @@
     export default {
         data() {
             return {
-                viewSettings: false,
-
                 settings: {
                     musicSettings: {
                         musicLink: 'https://www.youtube.com/watch?v=Hlp6aawXVoY'
@@ -59,9 +54,6 @@
         },
 
         methods: {
-            toggleSettings() {
-                this.viewSettings = !this.viewSettings;
-            },
             getCookie() {
                 let cookie = document.cookie;
                 let trimmedCookie = cookie.substring(9, cookie.length + 1);
@@ -81,8 +73,6 @@
                 console.log(document.cookie)
 
                 this.sendSettings();
-
-                this.toggleSettings();
             },
             sendSettings() {
                 this.$emit("get-cookie")
