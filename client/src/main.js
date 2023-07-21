c7c656b92e284f775f2dcad4c9cb1872c85e260f
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-21 10:39:30 -0400

    Set app to use google login

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import vue3GoogleLogin from "vue3-google-login";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(vue3GoogleLogin, {
+  clientId: `666230453304-k60nab5fc65gslhht108fqujrmli3lgf.apps.googleusercontent.com`,
+});
+
+app.mount("#app");
