deb3c227984d22e7ae7b96d48b1397cd85605ab9
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-21 17:30:41 -0400

    Update saveSettings() for cloud functions Reorder functions for organization

--- client/src/components/Settings.vue
+++ client/src/components/Settings.vue
@@ -73,21 +73,16 @@ export default {
     },
 
     methods: {
-        readCookie(name) {
-            let cookie = document.cookie;
-
-            let cookieStartIndex = cookie.indexOf(`${name}=`);
-            if (cookieStartIndex === -1) { return }
-            let cookieEndIndex = cookie.indexOf(';', cookieStartIndex);
-            if (cookieEndIndex === -1) { cookieEndIndex = cookie.length }
-
-            let cookieString = cookie.substring(cookieStartIndex + 9, cookieEndIndex);
-            let parsedCookie;
+        saveSettings() {
+            const SIGNED_IN = this.readCookie('id_token');
             
-            try { parsedCookie = JSON.parse(cookieString) }
-            catch (error) { return null }
+            if (SIGNED_IN) this.saveCloudSettings();
+            else this.saveCookieSettings(this.settings);
 
-            return parsedCookie;
+            this.sendSettings();
+        },
+        sendSettings() {
+            this.$emit("get-cookie")
         },
 
         getCookieSettings() {
@@ -107,14 +102,6 @@ export default {
             let dataString = JSON.stringify(data);
             
             document.cookie = `settings=${dataString}; expires= Sun, 1 January 2030 12:00:00 UTC; path=/`
-        },
-        saveSettings() {
-            this.saveCookieSettings(this.settings);
-
-            this.sendSettings();
-        },
-        sendSettings() {
-            this.$emit("get-cookie")
         },
 
         async getCloudSettings() {
@@ -163,6 +150,23 @@ export default {
                 console.error(error);
                 return null;
             }
+        }, 
+
+        readCookie(name) {
+            let cookie = document.cookie;
+
+            let cookieStartIndex = cookie.indexOf(`${name}=`);
+            if (cookieStartIndex === -1) { return }
+            let cookieEndIndex = cookie.indexOf(';', cookieStartIndex);
+            if (cookieEndIndex === -1) { cookieEndIndex = cookie.length }
+
+            let cookieString = cookie.substring(cookieStartIndex + 9, cookieEndIndex);
+            let parsedCookie;
+
+            try { parsedCookie = JSON.parse(cookieString) }
+            catch (error) { return null }
+
+            return parsedCookie;
         }
     },
 
