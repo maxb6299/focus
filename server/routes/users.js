5e7d77cc4676ef07000d1410559ca606c96663a2
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-21 17:17:20 -0400

    Improve backend to properly handle data

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,34 +1,65 @@
+const authenticate = require("../controllers/authenticate.js");
+
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  let data = [];
-  const database = req.app.get("database");
-
+router.get("/settings", async (req, res) => {
   try {
-    await database
+    const authHeader = req.headers.authorization;
+    const credential = authHeader.split(" ")[1];
+    const database = req.app.get("database");
+    const userId = await getUserId(credential);
+
+    const userData = await database
       .collection("users")
-      .find()
-      .forEach((element) => data.push(element));
+      .findOne({ userId: userId });
 
-    res.json(data);
-  } catch (err) {
-    res.json({ error: "Internal server error" });
-    console.log(err);
+    if (userData) {
+      res.json(userData.settings);
+      console.log("Successfully got user settings");
+    } else {
+      res.status(404).json({ error: "User not found" });
+      console.log("User not found");
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error getting user settings" });
+    console.log("Error getting user settings " + error);
   }
 });
 
-router.post("/", async (req, res) => {
-  const data = req.body;
-  const database = req.app.get("database");
-
+router.post("/settings", async (req, res) => {
   try {
-    const result = await database.collection("users").insertOne(data);
-    res.json(result);
-  } catch (err) {
-    res.json({ error: "Internal server error" });
-    console.log(err);
+    const authHeader = req.headers.authorization;
+    const credential = authHeader.split(" ")[1];
+    const database = req.app.get("database");
+    const userId = await getUserId(credential);
+    const userSettings = req.body.settings;
+
+    const userData = await database
+      .collection("users")
+      .updateOne(
+        { userId: userId },
+        { $set: { settings: userSettings } },
+        { upsert: true }
+      );
+
+    console.log(userData.settings);
+    console.log("Successfully updated user settings");
+  } catch (error) {
+    res.status(500).json({ error: "Error updating user settings" });
+    console.log("Error updating user settings " + error);
   }
 });
+
+async function getUserId(credential) {
+  try {
+    let result = await authenticate.verify_credentials(credential);
+    let userId = result.sub;
+
+    return userId;
+  } catch (error) {
+    throw new Error("Authentication failed");
+  }
+}
 
 module.exports = router;
