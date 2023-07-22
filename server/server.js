660386c58b6c0fd4de76d21b7ad5af1b5ddd6937
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-22 15:00:03 -0400

    Improve port variable in case it isnt in the .env

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 3000;
+
 const usersRoute = require("./routes/users");
 app.use("/users", usersRoute);
 
@@ -19,8 +21,8 @@ app.use((req, res) => {
 const { getDatabase, connectToDatabase } = require("./database");
 connectToDatabase((err) => {
   if (!err) {
-    app.listen(process.env.PORT, () =>
-      console.log(`Server started. Listening on port ${process.env.PORT}.`)
+    app.listen(PORT, () =>
+      console.log(`Server started. Listening on port ${PORT}.`)
     );
     const database = getDatabase();
     app.set("database", database);
