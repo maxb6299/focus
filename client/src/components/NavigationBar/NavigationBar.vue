c731996a2fb5dff1043f5c0d5c62403fd4178ea1
Max <126419161+maxb6299@users.noreply.github.com>
2023-08-10 15:45:41 -0400

    Implement hide navbar function

--- client/src/components/NavigationBar/NavigationBar.vue
+++ client/src/components/NavigationBar/NavigationBar.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="navbar">
-        <ButtonTemplate :buttonName="'Logo'" :isModalOn="false"></ButtonTemplate>
-        <ButtonTemplate :buttonName="'About'" :isModalOn="modalData.about.isModalOn" @buttonClicked="toggleModal"><AboutMenu></AboutMenu></ButtonTemplate>
-        <ButtonTemplate :buttonName="'Account'" :isModalOn="modalData.account.isModalOn" @buttonClicked="toggleModal"><AccountMenu></AccountMenu></ButtonTemplate>
-        <ButtonTemplate :buttonName="'Settings'" :isModalOn="modalData.settings.isModalOn" @buttonClicked="toggleModal"><SettingsMenu @savedSettings="toggleModal('Settings')"></SettingsMenu></ButtonTemplate>
+  <div class="navbar-wrapper" @mouseenter="showNavbar = true" @mouseleave="showNavbar = false">
+    <div class="navbar" v-if="showNavbar || settingsStore.getAppSettings.showNavbar || isModalOpen">
+      <ButtonTemplate :buttonName="'Logo'" :isModalOn="false"></ButtonTemplate>
+      <ButtonTemplate :buttonName="'About'" :isModalOn="modalData.about.isModalOn" @buttonClicked="toggleModal"><AboutMenu></AboutMenu></ButtonTemplate>
+      <ButtonTemplate :buttonName="'Account'" :isModalOn="modalData.account.isModalOn" @buttonClicked="toggleModal"><AccountMenu></AccountMenu></ButtonTemplate>
+      <ButtonTemplate :buttonName="'Settings'" :isModalOn="modalData.settings.isModalOn" @buttonClicked="toggleModal"><SettingsMenu @savedSettings="toggleModal('Settings')"></SettingsMenu></ButtonTemplate>
     </div>
+  </div>
 </template>
 
 <script>
@@ -12,6 +14,8 @@ import AboutMenu from '@/components/NavigationBar/AboutMenu.vue'
 import AccountMenu from '@/components/NavigationBar/AccountMenu.vue'
 import SettingsMenu from '@/components/NavigationBar/SettingsMenu.vue'
 import ButtonTemplate from '@/components/ButtonTemplate.vue'
+
+import { useSettingsStore } from "@/store/SettingsStore.js";
 
 export default {
     components: {
@@ -21,8 +25,16 @@ export default {
         SettingsMenu
     },
 
+    setup() {
+      const settingsStore = useSettingsStore();
+      return { settingsStore }
+    },
+
     data() {
       return {
+        showNavbar: false,
+        isModalOpen: false,
+
         modalData: {
           about: { isModalOn: false },
           account: { isModalOn: false },
@@ -46,6 +58,12 @@ export default {
           this.modalData.about.isModalOn = false;
           this.modalData.account.isModalOn = false;
         }
+
+        this.checkIfModalOpen();
+      },
+
+      checkIfModalOpen() {
+        this.isModalOpen =  this.modalData.about.isModalOn || this.modalData.account.isModalOn || this.modalData.settings.isModalOn;
       }
     }
 }
