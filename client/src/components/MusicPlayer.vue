418f535af348bfa16b2595602754797916b5ea73
Max <126419161+maxb6299@users.noreply.github.com>
2023-08-14 12:23:26 -0400

    Fix dots not showing in menu

--- client/src/components/MusicPlayer.vue
+++ client/src/components/MusicPlayer.vue
@@ -1,21 +1,41 @@
 <template>
-    <iframe width="400" :src=this.configureYoutubeLink(this.settingsStore.getMusicSettings.musicLink)></iframe>
+    <!-- <div>
+        <div v-show="!isPaused" id="player"></div>
+        <button @click="togglePlayback">{{ isPaused ? 'Play' : 'Pause' }}</button>
+    </div> -->
 </template>
 
 <script>
-import { useSettingsStore } from "@/store/SettingsStore.js";
 export default {
-    setup() {
-        const settingsStore = useSettingsStore();
-        return { settingsStore }
+    data() {
+        return {
+            isPaused: true,
+            player: null
+        };
     },
-
     methods: {
-        configureYoutubeLink(link) {
-            const linkBase = "https://www.youtube-nocookie.com/embed/"
-            let configuredLink = linkBase + link.substring(32, 43)
-            return configuredLink;
+        togglePlayback() {
+            if (this.isPaused) {
+                this.player = new window.YT.Player('player', {
+                    videoId: 'Hlp6aawXVoY', // Replace with the YouTube video ID you want to play
+                    events: {
+                        onReady: () => {
+                            this.player.playVideo();
+                        }
+                    }
+                });
+            } else {
+                this.player.pauseVideo();
+            }
+            this.isPaused = !this.isPaused;
         }
+    },
+    mounted() {
+        // Load YouTube Iframe Player API
+        const tag = document.createElement('script');
+        tag.src = 'https://www.youtube.com/iframe_api';
+        const firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
     }
-}
+};
 </script>
