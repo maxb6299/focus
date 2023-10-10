1e67870cfbc516e4946add346a8b3c23dce8d887
Max <126419161+maxb6299@users.noreply.github.com>
2023-10-10 10:57:08 +0200

    Write about section and update it's font size

--- client/src/components/NavigationBar/AboutMenu.vue
+++ client/src/components/NavigationBar/AboutMenu.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>
-        About Focus
+    <div class="about">
+        <h1>About Focus</h1>
+        <div>
+            Focus is a website for helping people with productivity. The goal was to make a minimal and distraction-free web application that still looked visually interesting. <br>
+            Some of the key features are: 
+            <ul>
+                <li> Pomodoro timer (a timer that has 25 minutes for work and 5 minutes for break)</li>
+                <li> An embedded Youtube playlist linking to classical music </li>
+                <li> Settings menu that customizes: timer length(s), what music link is playing, overall appearance</li>
+                <li> A sign in feature so the settings are stored in the cloud and can be accessed anywhere</li>
+            </ul>
+        </div>
     </div>
 </template>
